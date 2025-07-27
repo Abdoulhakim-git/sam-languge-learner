2dcd56438f3948e6c02e5b74f73569d739d76bcb
Abdoulhakim-git <abdoulhakim007@gmail.com>
2025-07-27 20:28:03 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -117,7 +117,7 @@ app.get("/api/manifest", (req, res) => {
   // It is the only port that is not firewalled.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log('Server is running on port ${PORT}');
 });
 })();
 
